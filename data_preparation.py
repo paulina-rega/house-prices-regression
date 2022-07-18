172700c92d1bd19aeee927b0321d25284412763a
paulina-rega <57419130+paulina-rega@users.noreply.github.com>
2022-07-18 17:04:07 +0200

    adding files

---
+++ data_preparation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 16 22:16:37 2022
+
+@author: paulinarega
+"""
+
+import pandas as pd
+
+
+
+df = pd.read_csv('train.csv')
+
+
