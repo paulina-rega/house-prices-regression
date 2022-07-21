22fa172310107ed95395af1a8b6675b83d892c10
paulina-rega <57419130+paulina-rega@users.noreply.github.com>
2022-07-21 12:05:00 +0200

    preprocessing training set

--- data_preparation.py
+++ data_preparation.py
@@ -113,6 +113,8 @@ f, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=.8, square=True);
 
 
+sns.heatmap(df_train.corr())
+
 
 #saleprice correlation matrix
 k = 10 #number of variables for heatmap
@@ -129,7 +131,135 @@ plt.show()
 # from correlation matrix: 
     #scatterplot
 sns.set()
-cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 
+        'FullBath', 'YearBuilt']
 sns.pairplot(df_train[cols], size = 2.5)
 plt.show();
+
+
+
+
+#missing data:
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(
+    ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+
+
+#dealing with missing data:
+df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index,1)
+df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
+#checking that there's no missing data missing:
+print(df_train.isnull().sum().max()) 
+
+
+
+
+
+#standardizing data
+saleprice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:,np.newaxis]);
+low_range = saleprice_scaled[saleprice_scaled[:,0].argsort()][:10]
+high_range= saleprice_scaled[saleprice_scaled[:,0].argsort()][-10:]
+print('outer range (low) of the distribution:')
+print(low_range)
+print('\nouter range (high) of the distribution:')
+print(high_range)
+
+
+
+
+
+#bivariate analysis saleprice/grlivarea
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+
+
+#deleting points
+df_train.sort_values(by = 'GrLivArea', ascending = False)[:2]
+df_train = df_train.drop(df_train[df_train['Id'] == 1299].index)
+df_train = df_train.drop(df_train[df_train['Id'] == 524].index)
+
+
+#bivariate analysis saleprice/grlivarea
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+
+
+
+
+#histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+
+#applying log transformation
+df_train['SalePrice'] = np.log(df_train['SalePrice'])
+
+
+#transformed histogram and normal probability plot
+sns.distplot(df_train['SalePrice'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+
+
+
+#histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+
+
+#data transformation
+df_train['GrLivArea'] = np.log(df_train['GrLivArea'])
+
+
+
+#transformed histogram and normal probability plot
+sns.distplot(df_train['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['GrLivArea'], plot=plt)
+
+
+
+
+
+#histogram and normal probability plot
+sns.distplot(df_train['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train['TotalBsmtSF'], plot=plt)
+
+
+
+#create column for new variable (one is enough because it's a binary categorical feature)
+#if area>0 it gets 1, for area==0 it gets 0
+df_train['HasBsmt'] = pd.Series(len(df_train['TotalBsmtSF']), index=df_train.index)
+df_train['HasBsmt'] = 0 
+df_train.loc[df_train['TotalBsmtSF']>0,'HasBsmt'] = 1
+
+#transform data
+df_train.loc[df_train['HasBsmt']==1,'TotalBsmtSF'] = np.log(df_train['TotalBsmtSF'])
+
+#histogram and normal probability plot
+sns.distplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], plot=plt)
+
+
+
+#scatter plot
+plt.scatter(df_train['GrLivArea'], df_train['SalePrice']);
+
+
+#scatter plot
+plt.scatter(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'], df_train[df_train['TotalBsmtSF']>0]['SalePrice']);
+
+
+
+#convert categorical variable into dummy
+df_train = pd.get_dummies(df_train)
 
